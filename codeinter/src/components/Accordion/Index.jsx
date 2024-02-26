7136d9d958bde2b3a6490fb6d1ead7b368dc4bbd
Sanjib Adhya <sanjib@zacksresearch.in>
2024-02-26 07:52:40 -0600

    completed react accordion

--- codeinter/src/components/Accordion/Index.jsx
+++ codeinter/src/components/Accordion/Index.jsx
@@ -5,7 +5,7 @@ import styles from "./style.module.css"
 export default function Accordion(){
     const [selected, setSelected] = useState(null)
     const[enableMultiSelection, setEnableMultiSelection] = useState(false)
-    const [multiSelect, setMultiSelect] = useState([])
+    const [multiple, setMultiple] = useState([])
 
     //single selection
     const handleSingleSelection = (getCurrentId)=>{
@@ -14,7 +14,19 @@ export default function Accordion(){
     }
 
     const handleMultiSelection = (getCurrentId)=>{
+        let cpyMultiple = [...multiple]
 
+        const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId)
+
+        console.log(findIndexOfCurrentId)
+
+        if(findIndexOfCurrentId === -1){
+            cpyMultiple.push(getCurrentId)
+        }else{
+            cpyMultiple.splice(findIndexOfCurrentId, 1)
+        }
+
+        setMultiple(cpyMultiple)
     }
 
     return(
@@ -32,7 +44,8 @@ export default function Accordion(){
                             <h3>{dataItem.question}</h3>
                             <span>+</span>
                         </div>
-                        {selected === dataItem.id ? <div className={styles.content}>{dataItem.answer}</div>: null}
+                        {enableMultiSelection ? multiple.indexOf(dataItem.id) !== -1 && <div className={styles.content}>{dataItem.answer}</div> : selected === dataItem.id && <div className={styles.content}>{dataItem.answer}</div>}
+                        {/* {selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? <div className={styles.content}>{dataItem.answer}</div>: null} */}
                     </div>) : <div>No data found</div>
                 }
             </div>
